855356bdb4471086b75d8f2c2961d239f90ac9c2
Harry James <harryjamesuk@users.noreply.github.com>
2016-10-01 18:35:20 +0100

    Bugfixes part 1
    Here's a few bugfixes, more are on their way!

--- ShoppingList.py
+++ ShoppingList.py
@@ -26,7 +26,6 @@ def askToAdd():
     item_to_add = input("> ")
     print("Attempting to add item...")
     if not addItem(item_to_add):
-        askToAdd()
         return False
     print("- Successfully added \"" + item_to_add + "\" to your Shopping List! :)")
     print("Press any key to continue...")
@@ -38,10 +37,13 @@ def askToAdd():
 def askToDelete():
     print("What would you like to delete? Provide item position or name.")
     item_to_delete = input("> ")
-    print("Ok, we'll delete", item_to_delete)
     print("Attempting to delete...")
-    deleteItem(item_to_delete)
-    print()
+    if not deleteItem(item_to_delete):
+        return False
+    print("- Successfully deleted \"" + item_to_delete + "\" from your Shopping List! :)")
+    print("Press any key to continue...")
+    input("> ")
+    print("\n\n\n")
     printShoppingList(False)
 
 
@@ -52,8 +54,13 @@ def askToReplace():
     print("Provide item name.")
     item_to_replace_with = input("> ")
     print("Attempting to replace...")
-    replaceItem(item_to_replace, item_to_replace_with)
-    print()
+    if not replaceItem(item_to_replace, item_to_replace_with):
+        askToReplace()
+        return False
+    print("- Successfully replaced \"" + item_to_replace + "\" with \"" + item_to_replace_with + "\"! :)")
+    print("Press any key to continue...")
+    input("> ")
+    print("\n\n\n")
     printShoppingList(False)
 
 
@@ -62,8 +69,10 @@ def askToGetpos():
     item_to_find = input("> ")
     print("Ok, we'll look for", item_to_find)
     print("Searching...")
-    getpos(item_to_find)
-    print()
+    if not getpos(item_to_find):
+        askToGetpos()
+        return False
+    print("\n\n\n")
     printShoppingList(False)
 
 
@@ -71,12 +80,15 @@ def askToPrintPos():
     print("What is the position of the item you are looking for? Provide item position.")
     item_to_print = input("> ")
     if item_to_print[0].isdigit():
-        printpos(item_to_print)
+        if not printpos(item_to_print):
+            askToPrintPos()
+            return False
     else:
         print("### ERROR ###")
         print("Please specify the numerical position of the item you are looking for.")
         askToPrintPos()
-    print()
+        return False
+    print("\n\n\n")
     printShoppingList(False)
 
 
@@ -114,13 +126,15 @@ def selectMenuOption():
 
 def returnIndexFromUserInput(user_input):
     user_input = user_input.strip()
-    for i in range(len(user_input)-1):
-        if i == 0:
-            index = user_input[i]
-        else:
-            index += user_input[i]
     if user_input[0].isdigit():
-        index = int(user_input)-1 # Convert to int, remove additional characters, and remove 1 to get index.
+        # Positional index
+        for i in range(len(user_input)):
+            if user_input[i].isdigit():
+                if i == 0:
+                    index = user_input[i]
+                else:
+                    index += user_input[i]
+        index = int(index)-1  # Convert to int and remove 1 to get index.
         if index < 0:
             print("### ERROR ###")
             print("The number you have specified is not a valid position in the shopping list. The shopping list",
@@ -151,10 +165,12 @@ def addItem(item):
     if any(char.isdigit() for char in item):
         print("### ERROR ###")
         print("Invalid usage: Please specify the name of the item you want to add.")
+        askToAdd()
         return False
     elif isinstance(item, str):
         shopping_list.append(item)
         return True
+    return False  # No if's reached.
 
 
 def deleteItem(item):
@@ -164,13 +180,18 @@ def deleteItem(item):
         # We've hit an error!
         print("Please try again:")
         askToDelete()
+        return False
     elif isinstance(index, int):
         del shopping_list[index]
+        return True
     elif isinstance(index, list):
-        for i in index:
+        for i in sorted(index, reverse=True):  # Ignore this error.
             del shopping_list[i]
+        return True
+    return False  # No if's reached
 
 
+# TODO: Handle where user provides a number as a replacement.
 def replaceItem(item, replacement):
     global shopping_list
     index = returnIndexFromUserInput(item)
@@ -178,11 +199,15 @@ def replaceItem(item, replacement):
         # We've hit an error!
         print("Please try again:")
         askToReplace()
+        return False
     elif isinstance(index, int):
         shopping_list[index] = replacement
+        return True
     elif isinstance(index, list):
         for i in index:
             shopping_list[i] = replacement
+        return True
+    return False  # No if's reached
 
 
 def getpos(item):
@@ -191,10 +216,13 @@ def getpos(item):
     if isinstance(index, bool):
         # We've hit an error!
         print("Please try again:")
-        getpos()
+        askToGetpos()
+        return False
     elif isinstance(index, list):
         for i in index:
             print("- Found:", item, "at position:", i + 1)
+        return True
+    return False  # No if's reached
 
 
 def printpos(item):
@@ -203,9 +231,12 @@ def printpos(item):
     if isinstance(index, bool):
         # We've hit an error!
         print("Please try again:")
-        askToDelete()
+        askToPrintPos()
+        return False
     elif isinstance(index, int):
         print("The item at position:", index+1, "is:", shopping_list[index])
+        return True
+    return False  # No if's reached
 
 # Startup
 name = input("What is your name? ")
